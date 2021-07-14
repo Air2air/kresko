9648d3d765aa53920815f57dfd57171fb1d008d5
Air2air <todd@dunningmarketing.com>
2021-07-14 16:10:36 -0700

    Latest

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
@@ -5,9 +7,15 @@ const Navbar = () => {
         <h2>Ninja List</h2>
       </div>
 
-      <a>Home</a>
-      <a>About</a>
-      <a>Ninja Listing</a>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ninjas">
+        <a>Ninja Listing</a>
+      </Link>
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
-
-
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
-          <Navbar />
           <h1>Homepage</h1>
           <p>
             Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit 
@@ -16,6 +13,8 @@ export default function Home() {
           <p>
             Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit Lorem Ipsum dolor sit 
           </p>
+          <Link href="/ninjas">See Ninja Listing</Link>
+  
         </div>
     )
 }
