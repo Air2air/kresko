f62cfce913be7fbb049eff47a30cd264ddda2e22
Air2air <todd@dunningmarketing.com>
2021-07-14 14:10:15 -0700

    Ninja

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h2>Ninja List</h2>
+      </div>
+
+      <a>Home</a>
+      <a>About</a>
+      <a>Ninja Listing</a>
+    </nav>
+  );
+};
+
+export default Navbar;
